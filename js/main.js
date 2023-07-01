ec04247c41f8f8bfccdefad0a403430538476590
Nataliia Turchyniak <nataturchyniak@gmail.com>
2023-07-01 22:15:16 +0300

    JS Start

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let userAcademy = 'Beetroot';
+console.log(userAcademy);
+
+let userCourses = 'Front-End';
+console.log(userCourses);
+
+let userCountry = 'Ukrain';
+console.log(userCountry);
+
+let fullName = 'Nataliia Turchyniak';
+console.log(fullName);
+
+let userSalary = '1000$';
+console.log(userSalary);
+
+let all = userAcademy + '\n' + userCourses + '\n' + userCountry + '\n' + fullName + '\n' + userSalary;
+console .log(all);
+console.log(userAcademy + '\n' + userCourses + '\n' + userCountry + '\n' + fullName + '\n' + userSalary);
